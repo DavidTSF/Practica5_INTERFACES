1eb1b55a9eeef33ede8fb39a63e426b94cc5014f
davvegtri <dvttsf@gmail.com>
2024-11-15 16:05:38 +0100

    Ejercicio 3 finalizado

--- El sol/app/src/main/java/dev/davidvega/elsol/CompararPlanetas.kt
+++ El sol/app/src/main/java/dev/davidvega/elsol/CompararPlanetas.kt
@@ -1,15 +1,70 @@
 package dev.davidvega.elsol
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.lang.String
+
 
 class CompararPlanetas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.comparar_planetas)
+
+
+        val listaPlanetas = arrayListOf(
+            Planeta( "Mercurio", 0.382, 0.387, 5400 ),
+            Planeta( "Venus", 0.949, 0.723, 5250 ),
+            Planeta( "Tierra", 1.0, 1.0, 5520 ),
+            Planeta( "Marte", 0.53, 1.542, 3960 ),
+            Planeta( "Júpiter", 11.2, 5.203, 1350 ),
+            Planeta( "Saturno", 9.41, 9.539, 700 ),
+            Planeta( "Urano", 3.38, 19.81, 1200 ),
+            Planeta( "Neptuno", 3.81, 30.07, 1500 ),
+            Planeta( "Plutón", null, 39.44, 5 )
+        )
+
+        val adapterIzq = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line, listaPlanetas
+        )
+
+        var autocompleteIzq = findViewById(R.id.autocomplete_izq) as AutoCompleteTextView
+
+        var diametroIzq = findViewById(R.id.diametro_izq) as TextView
+        var distanciasolIzq = findViewById(R.id.distanciasol_izq) as TextView
+        var densidadIzq = findViewById(R.id.densidad_izq) as TextView
+
+        autocompleteIzq.setOnItemClickListener { parent, view, position, id ->
+            val planetaSeleccionado = autocompleteIzq.getAdapter().getItem(position) as Planeta
+            diametroIzq.setText(planetaSeleccionado.diametro.toString())
+            distanciasolIzq.setText(String.valueOf(planetaSeleccionado.distanciaSol))
+            densidadIzq.setText(planetaSeleccionado.densidad.toString())
+        }
+        autocompleteIzq.setAdapter(adapterIzq)
+
+        val adapterDer = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line, listaPlanetas
+        )
+
+        var autocompleteDer = findViewById(R.id.autocomplete_der) as AutoCompleteTextView
+
+        var diametroDer = findViewById(R.id.diametro_der) as TextView
+        var distanciasolDer = findViewById(R.id.distanciasol_der) as TextView
+        var densidadDer = findViewById(R.id.densidad_der) as TextView
+
+        autocompleteDer.setOnItemClickListener { parent, view, position, id ->
+            val planetaSeleccionado = autocompleteDer.getAdapter().getItem(position) as Planeta
+            diametroDer.setText(planetaSeleccionado.diametro.toString())
+            distanciasolDer.setText(String.valueOf(planetaSeleccionado.distanciaSol))
+            densidadDer.setText(planetaSeleccionado.densidad.toString())
+        }
+        autocompleteDer.setAdapter(adapterDer)
+
+
 
     }
 }
--- El sol/app/src/main/java/dev/davidvega/elsol/Planeta.kt
+++ El sol/app/src/main/java/dev/davidvega/elsol/Planeta.kt
@@ -1,10 +1,30 @@
 package dev.davidvega.elsol
 
-import android.widget.ImageView
+class Planeta() {
+    var nombre = ""
+    var imageViewId: Int = 0
 
-class Planeta(nombre: String, image: Int) {
-    val nombre = nombre
-    val imageViewId: Int = image
+    var diametro = 0.0
+    var distanciaSol = 0.0
+    var densidad = 0
+
+    constructor(nombre: String, image: Int) : this() {
+        this.nombre = nombre
+        this.imageViewId = image
+    }
+
+    constructor(nombre: String, diametro: Double?, distanciaSol:Double, densidad: Int) : this() {
+        this.nombre = nombre
+        if (diametro != null) {
+            this.diametro = diametro
+        }
+        this.distanciaSol = distanciaSol
+        this.densidad = densidad
+    }
+
+    override fun toString(): String {
+        return nombre
+    }
 
 
 }
